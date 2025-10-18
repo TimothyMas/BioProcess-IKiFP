547d1a55c8ddb4611ea6b9205b3f8d32f8a84728
Timothy <150681899+TimothyMas@users.noreply.github.com>
2025-10-18 16:38:23 +0200

    Initialize tabs module with imports

---
+++ tabs/__init__.py
@@ -0,0 +1,3 @@
+# tabs/__init__.py
+from . import t01_planning, t02_simulation, t03_monitoring, t04_harvest
+from . import t05_activity, t06_bradford, t07_purification, t08_plots_export, t09_settings
